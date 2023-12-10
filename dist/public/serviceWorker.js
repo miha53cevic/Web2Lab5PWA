99c185f071f474b0903f1ad048f4cb95d8da035a
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2023-12-10 01:16:05 +0100

    radi cache strategija i offline nacin donekle

--- dist/public/serviceWorker.js
+++ dist/public/serviceWorker.js
@@ -1,59 +1,65 @@
 const cacheName = "diktafon-v1";
-const resourcesToCache = [
-    '/',
-    'index.html',
-    '404.html',
-    'index.css',
-    'logo.png',
-    'manifest.json',
+const cachableResources = [
+    "/",
+    "index.html",
+    "404.html",
+    "index.css",
+    "logo.png",
+    "manifest.json",
+    "offline.html",
+    "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js",
+    "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
 ];
 
-self.addEventListener('install', e => {
-    console.log("Instalacija service workera...");
-    e.waitUntil(async () => {
-        try {
-            const cache = await caches.open(cacheName);
-            await cache.addAll(resourcesToCache);
-        } catch(err) {
-            console.log("Greška kod instaliranja service workera!");
+async function Precache() {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(cachableResources);
+    console.log("Precache successful");
+}
+
+async function DeleteOldCaches() {
+    const keys = await caches.keys();
+    console.log("Current caches: ", keys);
+    for (const key of keys) {
+        if (key !== cacheName) {
+            await caches.delete(key);
+            console.log("Izbrisan cache: ", key);
         }
-    });
+    }
+}
+
+async function StaleWhileRevalidate(request) {
+    // Vrati cache ako postoji inače napravi request 
+    // te neovisno o tome je li poslan cache napravi request i osvježi cache
+    const response = await caches.match(request);
+    if (response) return response;
+    else {
+        try {
+            const requestResponse = await fetch(request);
+            if (requestResponse.status === 404) return await caches.match("404.html");
+            const cache = await caches.open(cacheName);
+            await cache.put(request.url, requestResponse.clone());
+            return requestResponse;
+        } catch (err) {
+            return await caches.match("offline.html");
+        }
+    }
+}
+
+self.addEventListener("install", e => {
+    console.log("Instalacija service workera...");
+    e.waitUntil(Precache());
 });
 
-self.addEventListener('activate', e => {
+
+self.addEventListener("activate", e => {
     console.log("Aktivacija service workera");
+    e.waitUntil(DeleteOldCaches());
 });
 
 self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches
-            .match(e.request)
-            .then((response) => {
-                if (response) {
-                    // console.log("Found " + event.request.url + " in cache!");
-                    //return response;
-                }
-                // console.log("----------------->> Network request for ",
-                //     event.request.url
-                // );
-                return fetch(e.request).then((response) => {
-                    // console.log("response.status = " + response.status);
-                    if (response.status === 404) {
-                        return caches.match("404.html");
-                    }
-                    return caches.open(staticCacheName).then((cache) => {
-                        // console.log(">>> Caching: " + event.request.url);
-                        cache.put(e.request.url, response.clone());
-                        return response;
-                    });
-                });
-            })
-            .catch((error) => {
-                console.log("Error", e.request.url, error);
-                // ovdje možemo pregledati header od zahtjeva i možda vratiti različite fallback sadržaje
-                // za različite zahtjeve - npr. ako je zahtjev za slikom možemo vratiti fallback sliku iz cachea
-                // ali zasad, za sve vraćamo samo offline.html:
-                return caches.match("offline.html");
-            })
-    );
+    e.respondWith(StaleWhileRevalidate(e.request));
 });
